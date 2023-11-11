b43c84d1f0956da1039175100d1905ef8daf2006
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2023-11-11 16:59:37 +0200

    Created data fetch hours.

--- data_manipulation/statistics.py
+++ data_manipulation/statistics.py
@@ -1,22 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import os
-import dotenv
-import supabase
 
-dotenv.load_dotenv()
+def compute_stats_work_days(data):
 
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
-
-client = supabase.create_client(url, key)
-response = client.table('User').select('id', 1).execute()
-
-def compute_stats(people):  # people
-    stats_ore_munca_total = people["ore_tasks"].describe()
-    stats_ore_invatare_curs_total = people["ore_curs"].describe()
-    stats_ore_invatare_personale_total = people["ore_inv"].describe()
 
 
 
--- utils/get_user_info.py
+++ utils/get_user_info.py
@@ -1,4 +1,4 @@
-def get_user_hours(user_id, tag_id, sb_client):
+def get_user_task_hours(user_id, tag_id, sb_client):
     user_skill = sb_client.table('User_Skill').select('*').eq('user', user_id).eq('tag', tag_id).execute().data[0]
     days_worked = sb_client.table('DailyWork').select('*').eq('user_skill', user_skill['id']).execute().data
 
@@ -7,3 +7,10 @@ def get_user_hours(user_id, tag_id, sb_client):
         days_hours_sum += day['hours']
 
     return user_skill['init_hours'] + days_hours_sum
+
+
+def get_user_task_days(user_id, tag_id, sb_client):
+    user_skill = sb_client.table('User_Skill').select('*').eq('user', user_id).eq('tag', tag_id).execute().data[0]
+    days_worked = sb_client.table('DailyWork').select('*').eq('user_skill', user_skill['id']).execute().data
+
+    return days_worked
